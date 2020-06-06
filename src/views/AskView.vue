48a2528f729fb27d021ad3d56c652251282874ea
wrallee <wrallee@naver.com>
2020-06-06 22:32:41 +0900

    ex3: list component applied

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,64 +1,14 @@
 <template>
-    <div>
-        <ul class="ask-list">
-            <li v-for="item in ask" class="post">
-                <!-- 포인트 영역 -->
-                <div class="points">
-                    {{ item.points }}
-                </div>
-                <!-- 기타 정보 영역 -->
-                <div>
-                    <p class="ask-title">
-                        <router-link :to="`/item/${item.id}`">
-                            {{ item.title }}
-                        </router-link>
-                    </p>
-                    <small class="link-text">
-                        {{ item.time_ago }} by
-                        <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
-                    </small>
-                </div>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <list-item></list-item>
+  </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-
+import ListItem from '../components/ListItem.vue'
 export default {
-    computed: {
-        ...mapState(['ask'])
-    },
-    created() {
-        this.$store.dispatch('FETCH_ASK');
-    }
+  components: {
+    ListItem,
+  }
 }
 </script>
-
-<style scoped>
-.ask-list {
-    margin: 0;
-    padding: 0;
-}
-.post {
-    list-style: none;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #eee;
-}
-.points {
-    width: 80px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #42b883;
-}
-.ask-title {
-    margin: 0;
-}
-.link-text {
-    color: #828282;
-}
-</style>
